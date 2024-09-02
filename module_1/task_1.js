0abeeeeaafb8e39611b9e1aa7c7e77aa1100d873
LeDaVinchi <davicnhi@gmail.com>
2024-09-02 16:34:04 +0500

    Update task_1.js

--- module_1/task_1.js
+++ module_1/task_1.js
@@ -1,1 +1,17 @@
-let = 0
+const prompt = (a,b,operator) => {
+    switch (a) {
+        case Number:
+            break;
+    }
+    if (operator === "+") {
+        return a + b
+    } else if (operator === "-") {
+        return a - b
+    } else if (operator === "*") {
+        return a * b
+    } else if (operator === "/") {
+        return a / b
+    }
+    return null
+}
+console.log(prompt(90,56,"/"));
