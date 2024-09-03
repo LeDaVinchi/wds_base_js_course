9aa042327569267452b4fdd134311b571e2b239d
LeDaVinchi <davicnhi@gmail.com>
2024-09-03 17:17:10 +0500

    Update task_5.js

--- module_1/task_5.js
+++ module_1/task_5.js
@@ -1,10 +1,13 @@
-const getTopStudents = (students, grade) => {
-    let students = [
-        { name: "Иван", grade: 85 },
-        { name: "Петя", grade: 95 },
-        { name: "Саша", grade: 70 }
-    ];
-    getTopStudents.filter(function(numder) {
-        return grade > 70;
-    });
+const students = [
+    { name: "Иван", grade: 85 },
+    { name: "Петя", grade: 95 },
+    { name: "Саша", grade: 70 }
+];
+const topStudens = [];
+
+for (let i = 0; i < students.length; i++) {
+    if (students[i] > 80) {
+        topStudens.push(students[i]);
     }
+}
+return topStudens;
